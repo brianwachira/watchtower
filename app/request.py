65c4de12062c5032dda55753e077ceda53270f22
brianwachira <brianwachira7@gmail.com>
2019-05-08 16:20:58 +0300

    add process results function

--- app/request.py
+++ app/request.py
@@ -18,6 +18,25 @@ def get_sources(Category):
                             language='en'
                             )
     
-    news_results = process_results(news_sources)
+    return news_results
+
+
+def process_results(news_sources_list):
+    '''
+    Function that processes the news result and transforms them to a list of objects
+    Args:
+        news_sources_list: A list of dictionaries that contain news details
+    Returns:
+            news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_sources_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+        description = news_item.get('description')
+        url = news_item.get('url')
+
+        news_object = News(id,name,description,url)
+        news_results.append(news_object)
 
     return news_results
