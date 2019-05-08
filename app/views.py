925fcee5d03fae5086f3873c2287f170bad6e046
brianwachira <brianwachira7@gmail.com>
2019-05-08 16:33:40 +0300

    add categories call

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 # Views
 @app.route('/')
@@ -9,6 +10,15 @@ def index():
     View root page function that returns the index page and its data
     '''
 
+    
+    general_news=get_sources('general')
+    business_news = get_sources('business')
+    entertainment_news =get_sources('entertainment')
+    health_news= get_sources('health')
+    science_news=get_sources('science')
+    sports_news=get_sources('sports')
+    technology_news=get_sources('technology')
+    
     title = 'Home - Welcome to Watch Tower'
     message = 'Hello World'
-    return render_template('index.html',message = message, title = title)
+    return render_template('index.html',message = message, title = title,general = general_news,business=business_news,entertainment=entertainment_news,health=health_news,science=science_news,sports=sports_news,technology=technology_news)
