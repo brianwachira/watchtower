bc5a8499aeacc55b551d10ebb3b806e06bed29ec
brianwachira <brianwachira7@gmail.com>
2019-05-08 16:00:40 +0300

    add get_sources function

--- app/request.py
+++ app/request.py
@@ -1,4 +1,23 @@
 from app import app
+from newsapi import NewsApiClient
+from .models import news
 
  # Getting api key
-api_key = app.config['NEWS_API_KEY']
+key = app.config['NEWS_API_KEY']
+
+
+
+def get_sources(Category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    newsapi = NewsApiClient(api_key=key)
+
+    news_sources = newsapi.get_sources(
+                            category= Category,
+                            language='en'
+                            )
+    
+    news_results = process_results(news_sources)
+
+    return news_results
